dbcd33bc75c948fc78ce9b0984719ef432cc1a57
9umpkin <2368322634@qq.com>
2024-03-23 11:36:43 +0800

    123

--- run_simple_demo.bat
+++ run_simple_demo.bat
@@ -6,6 +6,6 @@ set /a seed=!randHigh!+!randLow!
 echo Generated seed: !seed!
 pause
 
-.\PreliminaryJudge.exe -l ERR -s 123 -m maps\map10.txt .\Demo\avoid_main.exe
+.\PreliminaryJudge.exe -l ERR -m maps\map10.txt .\Demo\avoid_main_nofout.exe
 
 endlocal
